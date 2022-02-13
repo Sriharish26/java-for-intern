74bfa970bdb32aa576cda88292d6c7d5116e3a26
Sriharish26 <99585277+Sriharish26@users.noreply.github.com>
2022-02-13 19:25:29 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -4,7 +4,7 @@ public class factorial
 {
 public static void main(String args[])
 {
-Scanner sc=new scanner(System.in);
+Scanner sc=new Scanner(System.in);
 System.out.println("enter the number: ");
 int n=sc.nextInt();
 int result=1;
