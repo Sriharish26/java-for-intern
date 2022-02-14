b414595d3587934769fa3aa7219950875d0e31d5
Sriharish26 <99585277+Sriharish26@users.noreply.github.com>
2022-02-14 20:56:47 +0530

    Update and rename remove.java to removeelement.java

--- removeelement.java
+++ removeelement.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
-public class remove
+public class removeelement
 {
 public static void main(String[] args)
 {
 List<Integer>al=new ArrayList<>();
-a1.add(10);
-a1.add(20);
-a1.add(30);
+a1.add(15);
+a1.add(18);
+a1.add(22);
+a1.add(24);
 a1.add(1);
 a1.add(2);
 System.out.println(a1);
