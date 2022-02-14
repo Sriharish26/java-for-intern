ec961dd07c3d57431a1bb8bc3d82fb235a79ba9e
Sriharish26 <99585277+Sriharish26@users.noreply.github.com>
2022-02-14 12:18:18 +0530

    Create transpose.java

---
+++ transpose.java
@@ -0,0 +1,13 @@
+public class transpose
+{
+public static void main(String args[])
+{
+int original[][]={{1,3,4},{2,4,3},{3,4,5}};
+for(int i=0;i<3;i++)
+for(int j=0;j<3;j++)
+transpose[i][j]=original[j][i];
+}
+}
+System.out.println("Printing Matrix without transpose:");
+for(int i=0;i<3;i++)
+for(int j=0;j<)
